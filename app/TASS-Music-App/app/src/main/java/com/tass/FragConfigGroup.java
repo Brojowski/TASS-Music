76c192b68880c30a4b671f291f3a86335cedcacc
tdorn <tdorn99@yahoo.com>
2016-06-25 07:57:14 -0400

    Removed navigation (for the second time)

--- app/TASS-Music-App/app/src/main/java/com/tass/FragConfigGroup.java
+++ app/TASS-Music-App/app/src/main/java/com/tass/FragConfigGroup.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,54 +12,43 @@ import android.widget.EditText;
 
 import com.example.alex.tass_music_app.R;
 
-public class FragJoinGroup extends Fragment {
-    EditText playlistTextField;
-    EditText pinTextField;
+/**
+ * Created by Tyler on 5/8/2016.
+ */
+public class FragConfigGroup extends Fragment {
+
+    EditText txtGroupName = null;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return  inflater.inflate(R.layout.page_join_group, container, false);
-    }
-
-
-    public boolean sendRequest(String groupName){
-        return false;
-
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.page_config_group, container, false);
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
-        playlistTextField= (EditText) view.findViewById(R.id.group_name);
-        Button join_button = (Button) view.findViewById(R.id.join_button);
 
-
-        join_button.setOnClickListener(new View.OnClickListener() {
+        Button btnGroupConnect = (Button) view.findViewById(R.id.create_button);
+        txtGroupName = (EditText) view.findViewById(R.id.create_group_name);
+        btnGroupConnect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String groupName = txtGroupName.getText().toString();
 
+                try {
+                    // TODO: Pass group name to services
 
-                EditText group = (EditText) view.findViewById(R.id.group_name);
-
-
-                String Group = group.getText().toString();
-
-                if(sendRequest(Group)){
-
-                    Log.v("Does this shit work", "i love ice cream");
                     FragmentManager fragmentManager = getFragmentManager();
                     FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
                     fragmentTransaction.replace(R.id.app_content, new FragViewGroup());
                     fragmentTransaction.addToBackStack(null); // this may not be needed depending on how we want state preserved
                     fragmentTransaction.commit();
-                }else{
-
-                    group.setText("");
-
-
-                    playlistTextField.setText("");
-
+                } catch (Exception ex) {
+                    // TODO: HANDLE ME (unique group name?)
                 }
             }
         });
+
+
     }
 }
 
