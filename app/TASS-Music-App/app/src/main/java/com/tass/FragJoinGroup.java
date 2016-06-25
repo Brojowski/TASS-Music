25c417c00443adfca2524422fde12976a43d001f
tdorn <tdorn99@yahoo.com>
2016-06-25 04:44:52 -0400

    Added create page styling and controls.

--- app/TASS-Music-App/app/src/main/java/com/tass/FragJoinGroup.java
+++ app/TASS-Music-App/app/src/main/java/com/tass/FragJoinGroup.java
@@ -15,24 +15,26 @@ import com.example.alex.tass_music_app.R;
  * Created by Tyler on 5/8/2016.
  */
 public class FragJoinGroup extends Fragment {
-
-    private View inflatedView=null;
-    private Button join_button=null;
-    private EditText playlistTextField=null;
-    private EditText pinTextField=null;
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        //return inflater.inflate(R.layout.page_join_group, container, false);
-        inflatedView = inflater.inflate(R.layout.page_join_group, container, false);
-        join_button = (Button) inflatedView.findViewById(R.id.join_button);
-        playlistTextField= (EditText) inflatedView.findViewById(R.id.playlistNameTextField);
-        pinTextField = (EditText) inflatedView.findViewById(R.id.passwordTextField);
+        return  inflater.inflate(R.layout.page_join_group, container, false);
+    }
 
+    public boolean sendRequest(String playListName, String password){
+        return false;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+
+        Button join_button = (Button) view.findViewById(R.id.join_button);
         join_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                EditText playlistTextField= (EditText) view.findViewById(R.id.playlistNameTextField);
+                EditText pinTextField = (EditText) view.findViewById(R.id.passwordTextField);
+
                 String playListName= playlistTextField.getText().toString();
                 String password= pinTextField.getText().toString();
                 Log.v("Does this shit work", playListName+" "+password);
@@ -44,19 +46,6 @@ public class FragJoinGroup extends Fragment {
                 }
             }
         });
-        return inflatedView;
-    }
-
-    public boolean sendRequest(String playListName, String password){
-        return false;
-    }
-
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        // This event is triggered soon after onCreateView().
-        // Any view setup should occur here.  E.g., view lookups and attaching view listeners.
-        // Setup any handles to view objects here
-        // EditText etFoo = (EditText) view.findViewById(R.id.etFoo);
     }
 }
 
