015734b53a9c9fb84a2877656e5b1f411735f96d
tdorn <tdorn99@yahoo.com>
2016-06-25 04:55:22 -0400

    Fixed merge.

--- app/TASS-Music-App/app/src/main/java/com/tass/FragJoinGroup.java
+++ app/TASS-Music-App/app/src/main/java/com/tass/FragJoinGroup.java
@@ -16,39 +16,9 @@ import com.example.alex.tass_music_app.R;
  */
 public class FragJoinGroup extends Fragment {
 
-    private View inflatedView=null;
-    private Button join_button=null;
-    private EditText playlistTextField=null;
-    private EditText pinTextField=null;
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-<<<<<<< HEAD
-        // Inflate the layout for this fragment
-        //return inflater.inflate(R.layout.page_join_group, container, false);
-        inflatedView = inflater.inflate(R.layout.page_join_group, container, false);
-        join_button = (Button) inflatedView.findViewById(R.id.join_button);
-        playlistTextField= (EditText) inflatedView.findViewById(R.id.playlistNameTextField);
-        pinTextField = (EditText) inflatedView.findViewById(R.id.passwordTextField);
-
-        join_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String playListName= playlistTextField.getText().toString();
-                String password= pinTextField.getText().toString();
-                Log.v("Does this shit work", playListName+" "+password);
-                if(sendRequest(playListName,password)){
-                    Log.v("Does this shit work", "i love ice cream");
-                }else{
-                    playlistTextField.setText("");
-                    pinTextField.setText("");
-                }
-            }
-        });
-        return inflatedView;
-=======
         return  inflater.inflate(R.layout.page_join_group, container, false);
->>>>>>> origin/master
     }
 
     public boolean sendRequest(String playListName, String password){
