55497efd31de3f5a6927a19172abee27fde97a28
alex <alex.gajowski@gmail.com>
2016-06-25 11:01:30 -0400

    Update QUEUEITEM

--- app/TASS-Music-App/app/src/main/java/com/tass/services/QueueItem.java
+++ app/TASS-Music-App/app/src/main/java/com/tass/services/QueueItem.java
@@ -3,6 +3,29 @@ package com.tass.services;
 
 public class QueueItem
 {
-    public String ID;
-    public int Votes;
+    private String title;
+    private String Artist;
+    private String _uri;
+    private int _votes;
+
+    public QueueItem(String uri, int votes)
+    {
+        _uri = uri;
+        _votes = votes;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public String getAuthor()
+    {
+        return Artist;
+    }
+
+    private void getTrackInfo()
+    {
+        //resolve from the server here
+    }
 }
